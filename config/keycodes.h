3815e44661b7057b171248ced75baca53a2cdf44
Paweł Mizio <miziakmwa@gmail.com>
2022-12-27 22:42:24 +0100

    Update keycodes.h

--- config/keycodes.h
+++ config/keycodes.h
@@ -1,82 +1,79 @@
 #include <dt-bindings/zmk/keys.h>
-
+  
 // SPECIAL
-#define XXXX &none    
-#define ____ &trans    
+#define XXXX &none
+#define ____ &trans
 
 // ALPHANUM
 #define KC_A &kp A
-#define KC_B &kp B                         
-#define KC_C &kp C    
-#define KC_D &kp D    
-#define KC_E &kp E    
-#define KC_F &kp F    
-#define KC_G &kp G                         
+#define KC_B &kp B
+#define KC_C &kp C
+#define KC_D &kp D
+#define KC_E &kp E
+#define KC_F &kp F
+#define KC_G &kp G
 #define KC_H &kp H
-#define KC_I &kp I    
+#define KC_I &kp I
 #define KC_J &kp J
 #define KC_K &kp K
 #define KC_L &kp L
-#define KC_M &kp M    
-#define KC_N &kp N    
-#define KC_O &kp O    
-#define KC_P &kp P    
-#define KC_Q &kp Q    
-#define KC_R &kp R    
+#define KC_M &kp M
+#define KC_N &kp N
+#define KC_O &kp O
+#define KC_P &kp P
+#define KC_Q &kp Q
+#define KC_R &kp R
 #define KC_S &kp S
-#define KC_T &kp T                         
-#define KC_U &kp U    
-#define KC_V &kp V    
-#define KC_W &kp W    
-#define KC_X &kp X    
-#define KC_Y &kp Y    
-#define KC_Z &kp Z    
-#define KC_N &kp N1   
-#define KC_N &kp N2   
-#define KC_N &kp N3   
-#define KC_N &kp N4   
-#define KC_N &kp N5                        
-#define KC_N &kp N6   
-#define KC_N &kp N7   
-#define KC_N &kp N8   
-#define KC_N &kp N9   
-#define KC_N &kp N0
+#define KC_T &kp T
+#define KC_U &kp U
+#define KC_V &kp V
+#define KC_W &kp W
+#define KC_X &kp X
+#define KC_Y &kp Y
+#define KC_Z &kp Z
+#define KC_N1 &kp N1
+#define KC_N2 &kp N2
+#define KC_N3 &kp N3
+#define KC_N4 &kp N4
+#define KC_N5 &kp N5
+#define KC_N6 &kp N6
+#define KC_N7 &kp N7
+#define KC_N8 &kp N8
+#define KC_N9 &kp N9
+#define KC_N0 &kp N0
 
 // ARROWS
-#define KC_LEFT &kp LEFT 
-#define KC_DOWN &kp DOWN 
-#define KC_UP &kp UP   
+#define KC_LEFT &kp LEFT
+#define KC_DOWN &kp DOWN
+#define KC_UP &kp UP
 #define KC_RIGHT &kp RIGHT
 
-
 // SPECIAL CHARACTERS
-#define KC_AMPS &kp AMPS 
-#define KC_AT &kp AT   
+#define KC_AMPS &kp AMPS
+#define KC_AT &kp AT
 #define KC_BSLH &kp BSLH
-#define KC_BSLH &kp BSLH &none
 #define KC_BSPC &kp BSPC
-#define KC_CARET &kp CARET 
-#define KC_COMMA &kp COMMA 
-#define KC_DLLR &kp DLLR 
-#define KC_DOT &kp DOT 
-#define KC_EQUAL &kp EQUAL 
+#define KC_CARET &kp CARET
+#define KC_COMMA &kp COMMA
+#define KC_DLLR &kp DLLR
+#define KC_DOT &kp DOT
+#define KC_EQUAL &kp EQUAL
 #define KC_ESC &kp ESC
-#define KC_EXCL &kp EXCL 
+#define KC_EXCL &kp EXCL
 #define KC_FSLH &kp FSLH
-#define KC_HASH &kp HASH 
-#define KC_LBKT &kp LBKT 
-#define KC_LBRC &kp LBRC 
-#define KC_LPAR &kp LPAR 
+#define KC_HASH &kp HASH
+#define KC_LBKT &kp LBKT
+#define KC_LBRC &kp LBRC
+#define KC_LPAR &kp LPAR
 #define KC_MINUS &kp MINUS 
 #define KC_PIPE &kp PIPE
-#define KC_PLUS &kp PLUS 
-#define KC_PRCNT &kp PRCNT                     
-#define KC_RBKT &kp RBKT 
-#define KC_RBRC &kp RBRC 
+#define KC_PLUS &kp PLUS
+#define KC_PRCNT &kp PRCNT
+#define KC_RBKT &kp RBKT
+#define KC_RBRC &kp RBRC
 #define KC_RET &kp RET
 #define KC_RPAR &kp RPAR
 #define KC_SPACE &kp SPACE
 #define KC_SQT &kp SQT
-#define KC_STAR &kp STAR 
+#define KC_STAR &kp STAR
 #define KC_UNDER &kp UNDER
-#define KC_UNDER &kp UNDER 
