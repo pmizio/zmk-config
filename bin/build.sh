7f012dbc6e141218bab1ab7b63c1ae039eadd7b7
Paweł Mizio <mizio.pawel@gmail.com>
2023-11-12 14:12:06 +0100

    feat: lily58

--- bin/build.sh
+++ bin/build.sh
@@ -15,3 +15,17 @@ for SIDE in ${SIDES[@]}; do
 
   mv build/zephyr/zmk.uf2 "/firmware/${BOARD}.uf2"
 done
+
+for SIDE in ${SIDES[@]}; do
+  BOARD=lily58_${SIDE}
+
+  west build \
+    --pristine \
+    --source zmk/app \
+    --board nice_nano_v2 \
+    -- \
+    -DSHIELD="${BOARD}" \
+    -DZMK_CONFIG=/root/config
+
+  mv build/zephyr/zmk.uf2 "/firmware/${BOARD}.uf2"
+done
